05b1b2abfd3d063165165c88cba3ecbb0e3a2115
YOJI4910 <fmn.outlook@outlook.com>
2020-08-09 19:16:38 +0900

    [fix] read fontawesome css formerly

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,6 +5,11 @@ import Footer from "../components/footer"
 
 import "./layout.css"
 
+// fontawesomeのcssを先読みする
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
+
 export default ({ children }) => (
   <>
     <Header />
