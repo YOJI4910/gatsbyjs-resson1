506d053b2c08da236fc1c40adaa87750ba8ce491
YOJI4910 <fmn.outlook@outlook.com>
2020-08-23 19:12:39 +0900

    finished

---
+++ gatsby-node.js
@@ -0,0 +1,100 @@
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const blogresult = await graphql(`
+    query {
+      allContentfulBlogPost(sort: { fields: publishDate, order: DESC }) {
+        edges {
+          node {
+            id
+            slug
+          }
+          next {
+            title
+            slug
+          }
+          previous {
+            title
+            slug
+          }
+        }
+      }
+      allContentfulCategory {
+        edges {
+          node {
+            categorySlug
+            id
+            category
+            blogpost {
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (blogresult.errors) {
+    reporter.panicOnBuild(`GraphQL のクリエでエラーが発生しました`)
+  }
+
+  blogresult.data.allContentfulBlogPost.edges.forEach(
+    ({ node, next, previous }) => {
+      createPage({
+        path: `/blog/post/${node.slug}`,
+        component: path.resolve(`./src/templates/blogpost-template.jsx`),
+        context: {
+          id: node.id,
+          next,
+          previous,
+        },
+      })
+    }
+  )
+
+  const blogPostsPerPage = 6
+  const blogPosts = blogresult.data.allContentfulBlogPost.edges.length
+  const blogPages = Math.ceil(blogPosts / blogPostsPerPage)
+
+  Array.from({ length: blogPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog/` : `/blog/${i + 1}`,
+      component: path.resolve("./src/templates/blog-template.jsx"),
+      context: {
+        skip: blogPostsPerPage * i,
+        limit: blogPostsPerPage,
+        currentPage: i + 1,
+        isFirst: i + 1 === 1,
+        isLast: i + 1 === blogPages
+      }
+    })
+  })
+
+  blogresult.data.allContentfulCategory.edges.forEach(({ node }) => {
+    const catPostsPerPage = 6
+    const catPosts = node.blogpost.length
+    const catPages = Math.ceil(catPosts / carPostsPage)
+
+    Array.from({ length: catPages }).forEach((_, i) => {
+      createPage({
+        path:
+          i === 0
+            ? `/cat/${node.categorySlug}`
+            : `/cat/${node.categorySlu}g/${i + 1}`,
+        component: path.resolve(`./src/templates/cat-template.jsx`),
+        context: {
+          catid: node.id,
+          catname: node.category,
+          catslug: node.categorySlug,
+          skip: catPostsPerPage * i,
+          limit: catPostsPerPage,
+          currentPage: i + 1,
+          isFirst: i + 1 === 1,
+          isLast: i + 1 === catPages
+        }
+      })
+    })
+  })
+}
